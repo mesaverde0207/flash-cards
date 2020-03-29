7cda458f09e9088e24ef045b27d82c7300444765
y_fukuda <colors92window@gmail.com>
2020-03-30 03:02:19 +0900

    make a naive card component

--- src/app/flash/flash.component.ts
+++ src/app/flash/flash.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IFlash } from './../flash.model';
 
 @Component({
   selector: 'app-flash',
   templateUrl: './flash.component.html',
   styleUrls: ['./flash.component.css']
 })
-export class FlashComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FlashComponent {
+  @Input() flash: IFlash = {
+    id: 1,
+    question: 'React to Angular',
+    answer: 'No Reaction :)',
+    show: false,
+  };
 }
